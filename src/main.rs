96eae20085dfb8775e7193c4de774357fc45fd72
Peter Atashian <retep998@gmail.com>
2014-08-20 22:25:41 -0400

    Some refactoring.
    Signed-off-by: Peter Atashian <retep998@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,57 @@
+
 #![feature(phase)]
+
 #[phase(plugin)]
 extern crate regex_macros;
 extern crate regex;
+extern crate serialize;
 extern crate term;
 
+use std::io::fs::File;
 use std::io::net::tcp::TcpStream;
 use std::io::stdio::flush;
 use std::io::timer::sleep;
-use std::io::{BufferedReader, BufferedWriter, EndOfFile, IoResult};
+use std::io::{BufferedReader, BufferedWriter, EndOfFile, IoResult, IoError, InvalidInput, BufWriter};
+use std::sync::{Arc, Mutex};
 use std::time::Duration;
 use term::{Terminal, WriterWrapper};
 use term::stdout;
 
 struct Bot {
-term: Box<Terminal<WriterWrapper> + Send>,
-    cout: BufferedWriter<TcpStream>,
-    cin: BufferedReader<TcpStream>,
+    term: Box<Terminal<WriterWrapper> + Send>,
+    tcp: TcpStream,
 }
 impl Bot {
     fn connect() -> TcpStream {
         loop {
-            match TcpStream::connect("irc.esper.net", 6667) {
+            match TcpStream::connect("irc.fyrechat.net", 6667) {
                 Ok(tcp) => return tcp,
                 Err(e) => println!("Failed to connect: {}", e),
             }
         }
     }
-    fn new() -> Bot {
-        let tcp = Bot::connect();
-        println!("Connected!");
-        Bot {
+    fn new() -> Arc<Mutex<Bot>> {
+        Arc::new(Mutex::new(Bot {
             term: stdout().unwrap(),
-            cout: BufferedWriter::new(tcp.clone()),
-            cin: BufferedReader::new(tcp),
-        }
+            tcp: Bot::connect(),
+        }))
+    }
+    fn run(bot: Arc<Mutex<Bot>>) -> IoResult<()> {
+        let tcp = {
+            let mut bot = bot.lock();
+            try!(bot.send_nick("FTButt"));
+            try!(bot.send_user());
+            bot.tcp.clone()
+        };
+        Bot::read_loop(bot, BufferedReader::new(tcp))
     }
     // Borrowed from std::io::Buffer::read_until
-    fn read_line(&mut self) -> IoResult<Vec<u8>> {
+    fn read_line<T>(buf: &mut T) -> IoResult<Vec<u8>> where T: Reader + Buffer {
         let mut res = Vec::new();
         let mut used;
         loop {
             {
-                let available = match self.cin.fill_buf() {
+                let available = match buf.fill_buf() {
                     Ok(n) => n,
                     Err(ref e) if res.len() > 0 && e.kind == EndOfFile => {
                         used = 0;
@@ -61,107 +71,138 @@ impl Bot {
                     }
                 }
             }
-            self.cin.consume(used);
+            buf.consume(used);
         }
-        self.cin.consume(used);
+        buf.consume(used);
         Ok(res)
     }
-    fn parse_line(line: &str) -> (Option<&str>, Vec<&str>, Option<&str>) {
-        let reg_source = regex!(r"^:([^ ]+)");
-        let reg_msg = regex!(r" :(.*)$");
-        let reg_param = regex!(r"([^ ]+)");
-        let cap_source = reg_source.captures(line);
-        let cap_source = cap_source.as_ref();
-        let source = cap_source.map(|cap| cap.at(1));
-        let cap_msg = reg_msg.captures(line);
-        let cap_msg = cap_msg.as_ref();
-        let msg = cap_msg.map(|cap| cap.at(1));
-        let begin = cap_source.and_then(|cap| cap.pos(0)
-            .map(|pos| pos.val1())).unwrap_or(0);
-        let end = cap_msg.and_then(|cap| cap.pos(0)
-            .map(|pos| pos.val0())).unwrap_or(line.len());
-        let params = reg_param.captures_iter(line.slice(begin, end))
-            .map(|cap| cap.at(1)).collect::<Vec<&str>>();
-        (source, params, msg)
-    }
-    fn shorten(name: &str) -> &str {
-        match name.find('!') {
-            Some(pos) => name.slice_to(pos),
-            None => name,
-        }
-    }
-    fn run(&mut self) -> IoResult<()> {
-        try!(self.send_nick("FTButt"));
-        try!(self.send_user());
-        loop {
-            let line = try!(self.read_line());
-            let line = String::from_utf8_lossy(line.as_slice());
-            let (source, params, msg) = Bot::parse_line(line.as_slice());
-            let params = params.as_slice();
-            let command = match params.get(0) {
-                Some(command) => command,
-                None => continue,
-            };
-            let params = params.slice_from(1);
-            match command {
-                &"372" => {
-                    try!(self.term.fg(5));
-                    println!("{}", msg.unwrap_or(""));
-                }
-                &"375" => (),
-                &"376" => try!(self.send_join("#FTB-Wiki")),
-                &"433" => try!(self.send_nick("FTButtocks")),
-                &"NOTICE" => {
-                    match params.get(0) {
-                        Some(&"*") => {
-                            try!(self.term.fg(1));
-                            print!("NOTICE: ");
-                            flush();
-                            try!(self.term.fg(3));
-                            println!("{}", msg.unwrap_or(""));
-                        },
-                        Some(chan) => {
-                            try!(self.term.fg(9));
-                            print!("{} {} NOTICE: ", chan, Bot::shorten(source.unwrap_or("")));
-                            flush();
-                            try!(self.term.fg(11));
-                            println!("{}", msg.unwrap_or(""));
-                        },
-                        None => println!("HALP: {}", line),
-                    }
-                }
-                &"PRIVMSG" => {
-                    try!(self.term.fg(9));
-                    print!("{} {}: ", params.get(0).unwrap_or(&""), Bot::shorten(source.unwrap_or("")));
-                    flush();
-                    try!(self.term.fg(11));
-                    println!("{}", msg.unwrap_or(""));
-                },
-                &"PING" => try!(self.send_pong(msg)),
-                _ => {
-                    try!(self.term.fg(6));
-                    println!("{}, {}, {}, {}", source, command, params, msg);
-                },
+    fn parse_args(line: &str) -> Vec<&str> {
+        let reg = regex!(r" ([^: ]+)| :(.*)$");
+        reg.captures_iter(line).map(|cap| {
+            match cap.at(1) {
+                "" => cap.at(2),
+                x => x,
             }
+        }).collect()
+    }
+    fn parse_line(line: &str) -> IoResult<(Option<&str>, &str, Vec<&str>)> {
+        let reg = regex!(r"^(?::([^ ]+) )?([^ ]+)(.*)");
+        let cap = match reg.captures(line) {
+            Some(x) => x,
+            None => return Err(IoError {
+                kind: InvalidInput,
+                desc: "Failed to parse line",
+                detail: None,
+            }),
+        };
+        let source = match cap.at(1) { "" => None, x => Some(x) };
+        let command = cap.at(2);
+        let args = Bot::parse_args(cap.at(3));
+        Ok((source, command, args))
+    }
+    fn read_loop<T>(
+        bot: Arc<Mutex<Bot>>, mut buf: T
+    ) -> IoResult<()> where T: Reader + Buffer {
+        loop {
+            let line = try!(Bot::read_line(&mut buf));
+            if line.is_empty() { continue }
+            let line = String::from_utf8_lossy(line.as_slice());
+            let (source, command, args) = try!(Bot::parse_line(line.as_slice()));
+            try!(bot.lock().handle_command(source, command, args.as_slice()));
         }
+    }
+    fn handle_command(
+        &mut self, source: Option<&str>, command: &str, args: &[&str]
+    ) -> IoResult<()> {
+        match command {
+            "372" => { // MOTD
+                match args {
+                    [_, motd] => {
+                        try!(self.term.fg(5));
+                        println!("{}", motd);
+                    },
+                    _ => println!("CONFUSING MOTD: {}", args),
+                }
+            },
+            "375" => {}, // Begin of MOTD
+            "376" => { // End of MOTD
+                try!(self.send_join("#vana"));
+            },
+            "422" => {
+                try!(self.send_join("#vana"));
+            }, // Motd is missing
+            "433" => { // Nick already taken
+                try!(self.send_nick("FTButtocks"));
+            },
+            "NOTICE" => {
+                match args {
+                    ["*", msg] => {
+                        try!(self.term.fg(1));
+                        print!("NOTICE: ");
+                        flush();
+                        try!(self.term.fg(3));
+                        println!("{}", msg);
+                    },
+                    [chan, msg] => {
+                        try!(self.term.fg(9));
+                        print!("{} {} NOTICE: ", chan, source);
+                        flush();
+                        try!(self.term.fg(11));
+                        println!("{}", msg);
+                    },
+                    _ => println!("CONFUSING NOTICE: {}", args),
+                }
+            }
+            "PRIVMSG" => {
+                match args {
+                    [chan, msg] => {
+                        try!(self.term.fg(9));
+                        print!("{} {}: ", chan, source);
+                        flush();
+                        try!(self.term.fg(11));
+                        println!("{}", msg);
+                    },
+                    _ => println!("CONFUSING PRIVMSG: {}", args),
+                }
+
+            },
+            "PING" => {
+                match args {
+                    [msg] => try!(self.send_pong(Some(msg))),
+                    [] => try!(self.send_pong(None)),
+                    _ => println!("CONFUSING PING: {}", args),
+                }
+
+            },
+            _ => {
+                try!(self.term.fg(6));
+                println!("{}, {}, {}", source, command, args);
+            },
+        }
+        Ok(())
     }
     fn send(
-        &mut self, source: Option<&str>, command: &str, params: &[&str], msg: Option<&str>
+        &mut self, source: Option<&str>, command: &str, args: &[&str], msg: Option<&str>
     ) -> IoResult<()> {
-        match source {
-            Some(source) => try!(write!(self.cout, ":{} ", source)),
-            None => (),
-        }
-        try!(write!(self.cout, "{} ", command));
-        for param in params.iter() {
-            try!(write!(self.cout, "{} ", param));
-        }
-        match msg {
-            Some(msg) => try!(write!(self.cout, ":{}", msg)),
-            None => (),
-        }
-        try!(write!(self.cout, "\r\n"));
-        self.cout.flush()
+        let mut bufdata = [0, ..512];
+        let pos = {
+            let mut buf = BufWriter::new(bufdata);
+            match source {
+                Some(source) => try!(write!(buf, ":{} ", source)),
+                None => (),
+            }
+            try!(write!(buf, "{} ", command));
+            for arg in args.iter() {
+                try!(write!(buf, "{} ", arg));
+            }
+            match msg {
+                Some(msg) => try!(write!(buf, ":{}", msg)),
+                None => (),
+            }
+            try!(write!(buf, "\r\n"));
+            try!(buf.tell())
+        };
+        self.tcp.write(bufdata.slice_to(pos as uint))
     }
     fn send_nick(&mut self, nick: &str) -> IoResult<()> {
         self.send(None, "NICK", [nick], None)
@@ -179,11 +220,7 @@ impl Bot {
 
 fn main() {
     loop {
-        let mut bot = Bot::new();
-        match bot.run() {
-            Ok(_) => println!("Bot ended ok?"),
-            Err(e) => println!("Bot failed: {}", e),
-        }
-        sleep(Duration::seconds(10));
+        let bot = Bot::new();
+        println!("{}", Bot::run(bot));
     }
 }
